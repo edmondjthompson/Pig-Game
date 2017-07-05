f8063242f8ba7cf02b881b3b9c80dd197c9b9dd5
Edmond Thompson <edmond.j.thompson@gmail.com>
2017-07-05 16:23:39 -0400

    Added event handlers for dice rolls

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
@@ -18,11 +18,35 @@ activePlayer = 0;
 //generate random number
 dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+
+//change CSS property with querySelector
+document.querySelector(".dice").style.display = "none";
+
+//set scores to 0
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+//event handlers
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2 . Display result
+    var diceDOM = document.querySelector(".dice")
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+
+    // 3. Update round score IF the rolled number was NOT a 1
+
+});
+
+
+
+
+//document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
 
 //read from document
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
-
-document.querySelector(".dice").style.display = "none";
+//var x = document.querySelector("#score-0").textContent;
